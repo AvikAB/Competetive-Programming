322c3719d509ec0326b0781439deca7caf85cc48
Avik <119071924+AvikAB@users.noreply.github.com>
2025-09-17 23:14:39 +0600

    Create Basic Linked List Traversal.cpp

---
+++ Linked List/Basic Linked List Traversal.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long int
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+struct Node {
+    public:
+        int data;
+        Node* next;
+
+    public:
+        // constructor with default next = nullptr
+        Node (int data1, Node* next1=nullptr){
+            data = data1;
+            next = next1;
+        }
+};
+
+int main(){
+    FASTER
+
+    vector<int>arr = {2,5,8,7};
+    // create head node
+    Node* head = new Node(arr[0], nullptr);
+    Node* temp = head;
+    // link the rest
+    for(int i=1; i<arr.size(); i++){
+        temp -> next = new Node(arr[i]);
+        temp = temp -> next;
+    }
+    // print the linked list
+    temp = head;
+    while(temp != nullptr){
+        cout<<temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout<<nl;
+}
