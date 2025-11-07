b5e3cd17108e988f8e1907e6c26baee15c97a354
Avik <119071924+AvikAB@users.noreply.github.com>
2025-11-07 13:04:40 +0600

    Implement recursive subsequence generation
    Added a function to generate subsequences of a string using recursion.

--- Recursion/Subsequence Generate.cpp
+++ Recursion/Subsequence Generate.cpp
@@ -91,3 +91,41 @@ int main(){
         cout<<nl;
     }
 }
+
+// Subsequence Generate using string:
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long int
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+void subSeq(string res, ll pos, string s){
+    if(pos==s.size()){
+        cout<<res<<nl;
+        return;
+    }
+    subSeq(res, pos+1, s);  // exclude
+    res = res+s[pos];  // include
+    subSeq(res, pos+1, s);
+}
+
+int main(){
+    FASTER
+
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string res="";
+    subSeq(res, 0, s);
+}
