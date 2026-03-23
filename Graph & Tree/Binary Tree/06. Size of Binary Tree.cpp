0149db1d9b2408d24af5aee4008f7d9f4888ffed
Avik <119071924+AvikAB@users.noreply.github.com>
2026-03-24 02:14:19 +0600

    Update 06. Size of Binary Tree.cpp

--- Graph & Tree/Binary Tree/06. Size of Binary Tree.cpp
+++ Graph & Tree/Binary Tree/06. Size of Binary Tree.cpp
@@ -71,3 +71,74 @@ int main(){
     PostOrder(root, cnt);
     cout<<cnt<<nl;
 }
+
+
+// Mrthod 2: Direct Left+Right+1
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+class Node {
+public:
+    int data;
+    Node *left, *right ;   // pointer of left, right
+
+    // Constructor to initialize the node with a value
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// using count:
+int getSize(Node* node){
+    if(node==NULL) return 0;
+    return (1+getSize(node->left)+getSize(node->right));
+}
+
+int main(){
+    int x, first, second;   // root, left val, right val
+    cout<<"Enter root value: ";
+    cin>>x;
+    queue<Node*>q;   // queue stores node's pointers
+    Node *root = new Node(x);    // creating a root node of x
+    q.push(root);
+
+    // build the binary tree
+    while(!q.empty()){
+        Node *temp = q.front();
+        q.pop();
+
+        // left node:
+        cout<<"Enter left child of "<<temp->data<<": ";
+        cin>>first;   // val of left node
+        if(first!=-1){
+            temp->left = new Node(first);   // creating a new node of left & the value is first
+            q.push(temp->left);      // push the val into queue
+        }
+
+        // right node:
+        cout<<"Enter right child of "<<temp->data<<": ";
+        cin>>second;
+        if(second!=-1){
+            temp->right = new Node(second);    // creating a new node of right & the value is second
+            q.push(temp->right);    // push the val into queue
+        }
+    }
+    cout<<"Tree Successfully Created!"<<nl;
+
+    
+    cout<<getSize(root)<<nl;
+}
