a3bbe1a7268c07169dec90aa8443cd358f9f5072
Avik <119071924+AvikAB@users.noreply.github.com>
2026-04-07 00:29:06 +0600

    Create 17. Left View of Binary Tree.cpp

---
+++ Graph & Tree/Binary Tree/17. Left View of Binary Tree.cpp
@@ -0,0 +1,89 @@
+// Left View of Binary Tree:
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+class Node {
+public:
+    ll data;
+    Node *left, *right ;   // pointer of left, right
+
+    // Constructor to initialize the node with a value
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+vector<ll> leftView(Node* root){   // level order traversal
+    queue<Node*>q;
+    q.push(root);
+    vector<ll>ans;
+    while(!q.empty()){
+        ll lvlsz = q.size();
+        ans.push_back(q.front()->data);
+        while(lvlsz--){   // then the other ele are not so important, just check their children - thats why remove them
+            Node* temp = q.front();
+            q.pop();
+
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    ll x, first, second;   // root, left val, right val
+    cout<<"Enter root value: ";
+    cin>>x;
+    queue<Node*>q;   // queue stores node's pointers
+    Node *root = new Node(x);    // creating a root node of x
+    q.push(root);
+
+    // build the binary tree
+    while(!q.empty()){
+        Node *temp = q.front();
+        q.pop();
+
+        // left node:
+        cout<<"Enter left child of "<<temp->data<<": ";
+        cin>>first;   // val of left node
+        if(first!=-1){
+            temp->left = new Node(first);   // creating a new node of left & the value is first
+            q.push(temp->left);      // push the val into queue
+        }
+
+        // right node:
+        cout<<"Enter right child of "<<temp->data<<": ";
+        cin>>second;
+        if(second!=-1){
+            temp->right = new Node(second);    // creating a new node of right & the value is second
+            q.push(temp->right);    // push the val into queue
+        }
+    }
+    cout<<"Tree Successfully Created!"<<nl;
+
+    vector<ll> ans = leftView(root);
+    for(auto av:ans){
+        cout<<av<<" ";
+    }
+    cout<<nl;
+}
+
+
+// TC: O(n)
+// SC: O(n)
