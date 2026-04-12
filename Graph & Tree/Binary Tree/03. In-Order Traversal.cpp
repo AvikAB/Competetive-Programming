e7df9a098c4657ca6193d94b7969914706491a9d
Avik <119071924+AvikAB@users.noreply.github.com>
2026-04-13 02:18:55 +0600

    Update 03. In-Order Traversal.cpp

--- Graph & Tree/Binary Tree/03. In-Order Traversal.cpp
+++ Graph & Tree/Binary Tree/03. In-Order Traversal.cpp
@@ -1,4 +1,4 @@
-// Binary Tree
+// Method 1: using recusrsion
 
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
@@ -75,3 +75,109 @@ int main(){
 /*
 TC: O(n)
 */
+
+// Method 2: using iterative way
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+class Node {
+public:
+    ll data;
+    Node *left, *right ;   // pointer of left, right
+
+    // Constructor to initialize the node with a value
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+vector<ll>inorder(Node* root){  // LNR, but here RNL for stack
+    stack<Node*>s;
+    stack<bool>visited;
+    s.push(root);
+    visited.push(0);
+    vector<ll>ans;
+
+    while(!s.empty()){
+        Node* temp = s.top();
+        s.pop();
+        bool flag = visited.top();
+        visited.pop();
+        
+        // first time visit -> RNL
+        if(flag==0){
+            // Right
+            if(temp->right){ 
+                s.push(temp->right);
+                visited.push(0);
+            }
+            // Node
+            s.push(temp);
+            visited.push(1);
+            // Left
+            if(temp->left){
+                s.push(temp->left);
+                visited.push(0);
+            }
+        } 
+        
+        // second time visited
+        else ans.push_back(temp->data);
+    }
+    return ans;
+}
+
+int main(){
+    ll x, first, second;   // root, left val, right val
+    cout<<"Enter root value: ";
+    cin>>x;
+    queue<Node*>q;   // queue stores node's pointers
+    Node *root = new Node(x);    // creating a root node of x
+    q.push(root);
+
+    // build the binary tree
+    while(!q.empty()){
+        Node *temp = q.front();
+        q.pop();
+
+        // left node:
+        cout<<"Enter left child of "<<temp->data<<": ";
+        cin>>first;   // val of left node
+        if(first!=-1){
+            temp->left = new Node(first);   // creating a new node of left & the value is first
+            q.push(temp->left);      // push the val into queue
+        }
+
+        // right node:
+        cout<<"Enter right child of "<<temp->data<<": ";
+        cin>>second;
+        if(second!=-1){
+            temp->right = new Node(second);    // creating a new node of right & the value is second
+            q.push(temp->right);    // push the val into queue
+        }
+    }
+    cout<<"Tree Successfully Created!"<<nl;
+
+    vector<ll> ans = inorder(root);
+    for(auto &av:ans){
+        cout<<av<<" ";
+    }
+    cout<<nl;
+}
+
+
+// TC: O(n)
+// SC: O(n)
