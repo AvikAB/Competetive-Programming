904e74d8a1e884fb59bc973fdf6169069a5db9ee
Avik <119071924+AvikAB@users.noreply.github.com>
2026-04-26 22:25:15 +0600

    Add binary tree construction from traversals
    Implement binary tree construction from preorder and inorder traversals with level order output.

---
+++ Graph & Tree/Binary Tree/21. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,105 @@
+// Method 1: (using mathematical expression of idx+(pos-InSt)+1 of Preorder for right subtree)
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define nl "\n"
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+template <typename T> using ordered_set = tree<T,null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 1e5+10;
+const ll mod = 1e9+7;
+const ll INF = 1e9+10;
+
+class Node {
+public:
+    ll data;
+    Node *left, *right ;   // pointer of left, right
+
+    // Constructor to initialize the node with a value
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+ll find(vector<ll>&inorder, ll target, ll start, ll end){
+    for(int i=start; i<=end; i++){
+        if(inorder[i]==target) return i;
+    }
+    return -1;
+}
+
+Node* Tree(vector<ll>&inorder, vector<ll>&preorder, ll InSt, ll InEnd, ll idx){
+    if(InSt>InEnd) return NULL;  // base case
+
+    Node *root = new Node(preorder[idx]);   // create a new root using preorder[idx] val
+    ll pos = find(inorder, preorder[idx], InSt, InEnd);
+
+    // Build left tree
+    root->left = Tree(inorder, preorder, InSt, pos-1, idx+1);
+    // Build right tree
+    root->right = Tree(inorder, preorder, pos+1, InEnd, idx+(pos-InSt)+1);
+    return root;
+}
+
+vector<ll>levelOrder(Node* root){
+    vector<ll>ans;
+    if(root==NULL) return ans;
+
+    queue<Node*>q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node *curr = q.front();
+        q.pop();
+
+        if(curr){
+            ans.push_back(curr->data);
+            q.push(curr->left);
+            q.push(curr->right);
+        } else ans.push_back(-1);
+    }
+    
+    // Remove trailing nulls
+    while(!ans.empty() and ans.back()==-1){
+        ans.pop_back();
+    }
+    return ans;
+}
+
+int main(){
+    // Direct input method
+    ll n;
+    cout<<"Enter number of nodes: ";
+    cin>>n;
+    
+    vector<ll> preorder(n), inorder(n);
+    
+    cout<<"Enter preorder traversal: ";
+    for(int i=0; i<n; i++) cin>>preorder[i];
+    
+    cout<<"Enter inorder traversal: ";
+    for(int i=0; i<n; i++) cin>>inorder[i];
+    
+    // Build tree
+    ll preOidx = 0;
+    Node* root = Tree(inorder, preorder, 0, n-1, preOidx);  // (inorder, preorder, start_of_inorder, end_of_inorder, start_index_of_preorder)
+    
+    // Get level order traversal
+    vector<ll> level = levelOrder(root);
+    
+    cout<<"Level Order Traversal (Output): ";
+    for(int i=0; i<level.size(); i++){
+        if(level[i]==-1) cout<<"null, ";
+        else cout<<level[i]<<", ";
+    }
+    cout<<nl;
+}
+
+// TC: O(n^2)
+// SC: O(n)
